72d70d52ca58c3514709f9b5f61721337695a809
sheral1610 <bsheral1610@gmail.com>
2022-07-21 15:23:54 +0530

    Create interest.java
    created simple interest program

---
+++ interest.java
@@ -0,0 +1,13 @@
+class interest 
+{
+public static void main(String args[])
+{
+int p= 5000;
+int t= 10;
+int r=200;
+System.out.println("Principle is "+p);
+System.out.println("Time is "+t);
+System.out.println("Rate is "+r);
+System.out.println("calculate simple interest"+(p*t*r)/100);
+}
+}
