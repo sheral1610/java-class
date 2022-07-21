583145b55f1195db1afc4ad1cb7acfc95a479762
sheral1610 <bsheral1610@gmail.com>
2022-07-21 17:07:11 +0530

    Update datatype.java
    datatype

--- datatype.java
+++ datatype.java
@@ -5,8 +5,18 @@ public static void main(String args[])
 int a=10;
 float b= 20.5f;
 char c= 'a';
+long d=20;
+double e=30.6;
+short g=4;
+boolean h=true;
+byte i=1;
 System.out.println("value of variable is "+a+" size of int "+Integer.BYTES);
 System.out.println("value of variable is "+b+" size of float "+Float.BYTES);
 System.out.println("value of variable is "+c+" size of char "+Character.BYTES);
+System.out.println("value of variable is "+d+" size of long "+Long.BYTES);
+System.out.println("value of variable is "+e+" size of double "+Double.BYTES);
+System.out.println("value of variable is "+g+" size of short "+Short.BYTES);
+System.out.println("value of variable is "+h);
+System.out.println("value of variable is "+i+" size of byte "+Byte.BYTES);
 }
 }
