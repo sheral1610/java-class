d0a8173bffbe89f6a9ee8a66c826fdd7e4e1e836
sheral1610 <bsheral1610@gmail.com>
2022-07-21 16:47:19 +0530

    datatype
    created datatypes program

---
+++ datatype.java
@@ -0,0 +1,12 @@
+class datatype
+{
+public static void main(String args[])
+{
+int a=10;
+float b= 20.5f;
+char c= 'a';
+System.out.println("value of variable is "+a+" size of int "+Integer.BYTES);
+System.out.println("value of variable is "+b+" size of float "+Float.BYTES);
+System.out.println("value of variable is "+c+" size of char "+Character.BYTES);
+}
+}
---
+++ statements.java
@@ -0,0 +1,11 @@
+class statements
+{
+public static void main(String args[])
+{
+int a=100;
+if (a<100)
+System.out.println("less than 100"+a);
+else
+System.out.println("incorrect value");
+}
+}
