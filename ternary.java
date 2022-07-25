f93474e533de1500a1e12bbce8d2f4129dd3ae73
sheral1610 <bsheral1610@gmail.com>
2022-07-25 15:10:56 +0530

    Create ternary.java
    created ternary operator

---
+++ ternary.java
@@ -0,0 +1,12 @@
+class Ternary
+{
+public static void main(String args[])
+{
+int x=5;
+int y=6;
+int z=7;
+String result = x==7 ? "needed" : "not needed";
+System.out.println("Result is "+result);
+
+}
+}
