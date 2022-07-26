2d67d7c66ab797a80f3c7fcfbe339d29a937af3d
sheral1610 <bsheral1610@gmail.com>
2022-07-26 13:10:38 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -3,9 +3,17 @@ class Pattern
 public static void main(String args[])
 {
 int i,j, row=5;
-for( i=0; i<5; i++){
+for( i=0; i<=5-1; i++){
 for( j=0; j<=i ; j++){
-System.out.print("x");
+System.out.print("x"+" ");
+}
+System.out.print("\n");
+}
+for(i=5-1; i>=0; i--)
+{
+for(j=0;j<=i-1; j++)
+{
+System.out.print("x"+" ");
 }
 System.out.print("\n");
 }
