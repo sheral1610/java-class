b2139d6ce8308c2508ffae1c26accbb5a05f94e6
sheral1610 <bsheral1610@gmail.com>
2022-07-26 13:03:21 +0530

    pattern

---
+++ Pattern.java
@@ -0,0 +1,13 @@
+class Pattern
+{
+public static void main(String args[])
+{
+int i,j, row=5;
+for( i=0; i<5; i++){
+for( j=0; j<=i ; j++){
+System.out.print("x");
+}
+System.out.print("\n");
+}
+}
+}
---
+++ looping statements.java
@@ -0,0 +1,10 @@
+class LoopingStatements
+{
+public static void main(String args[])
+{
+for(int i=5; i<1; i--){
+for(int j=0; j<i ; j++){
+System.out.print("x");
+}
+System.out.println("/n");
+}}}
