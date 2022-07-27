e785f549f435a9736ac42aa8484780706cb58c44
sheral1610 <bsheral1610@gmail.com>
2022-07-27 17:05:56 +0530

    Update twodarray.java

--- twodarray.java
+++ twodarray.java
@@ -8,8 +8,8 @@ for(int j=0;j<twod.length;j++){
 twod[i][j]=i+j;
 }}
 for(int i=0;i<twod.length;i++){
-for(int j=0;i<twod.length;j++){
-System.out.println(twod[i][j]+" ");
+for(int j=0;j<twod.length;j++){
+System.out.print(twod[i][j]+" ");
 }
 System.out.println(" ");
 }
